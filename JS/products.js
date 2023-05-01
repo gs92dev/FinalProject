6927120f3c2c6b1f90171cc00a30700d0b66f53f
Guilherme dos Santos <gs92dev@gmail.com>
2023-05-01 14:35:20 -0500

    Form changes

--- JS/products.js
+++ JS/products.js
@@ -14,37 +14,50 @@ products.addItem(
   "img/products/pexels-koolshooters-8945936.jpg"
 );
 
-/*
 // Select the New Task Form
-const newItemForm = document.querySelector('#newItemForm');
+const newItemForm = document.querySelector("#itemForm");
 
 // Add an 'onsubmit' event listener
-newItemForm.addEventListener('submit', (event) => {
-    // Prevent default action
-    event.preventDefault();
+newItemForm.addEventListener("submit", (event) => {
+  // Prevent default action
+  event.preventDefault();
 
-    // Select the inputs
-    const newItemNameInput = document.querySelector('#newItemNameInput');
-    const newItemDescription = document.querySelector('#newItemDescription');
+  // Select the inputs
+  const newItemNameInput = document.querySelector("#newItemNameInput");
+  const newItemDescription = document.querySelector("#newItemDescription");
+  const newItemPrice = document.getElementById("newItemPrice");
+  const newItemImg = document.getElementById("newItemImg");
 
-    
-     //   Validation code here
-    
+  //   Validation code here
 
-    // Get the values of the inputs
-    const name = newItemNameInput.value;
-    const description = newItemDescription.value;
-    const createdAt = new Date();
+  // Get the values of the inputs
+  const price = newItemPrice.value;
+  const name = newItemNameInput.value;
+  const description = newItemDescription.value;
+  const createdAt = new Date();
+  const img = newItemImg.value;
 
-    // Add the task to the task manager
-    itemsController.addItem(name, description, createdAt);
+  /*const reader = new FileReader();
+  reader.addEventListener("load", (event) => {
+    const imageDataUrl = event.target.result;
+    console.log("Image data URL:", imageDataUrl);
+    // TODO: do something with the image data URL   
+  });
 
-    // Clear the form
-    newItemNameInput.value = '';
-    newItemDescription.value = '';
-}); */
+  reader.readAsDataURL(img);*/
 
-export function addItemCard(item) {
+  console.log(name, price, description, createdAt, img);
+  // Add the task to the task manager
+  // itemsController.addItem(name, description, createdAt);
+
+  // Clear the form
+  newItemNameInput.value = "";
+  newItemDescription.value = "";
+  newItemPrice.value = "";
+  newItemImg.value = "";
+});
+
+/*export function addItemCard(item) {
   const itemHTML =
     `<div class="card" style="width: 18rem">
   <img
@@ -67,4 +80,4 @@ export function addItemCard(item) {
 }
 console.log(products.items[1]);
 addItemCard(products.items[1]);
-addItemCard(products.items[0]);
+addItemCard(products.items[0]);*/
